2866ffcf73f51795a1446a36401115b5c85241f7
YurGoV <yurgov@gmail.com>
2022-10-06 09:18:31 +0300

    1st task draft

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryCards = galleryItems.map(({description, original, preview}) => 
+  `<a class="gallery__item" href="${original}">
+  <img 
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+  />
+</a>`)
+.join("");
+
+
+const refGallery = document.querySelector('.gallery')// шукаємо дів-ку
+// console.log(refGallery);
+
+refGallery.insertAdjacentHTML('beforeend', galleryCards)// аджастимо галерею в ДОМ
+
+refGallery.addEventListener('click', openModal);//Додаємо слухача на gallery (галарею в цілому), та виклик SimpleLightbox
+
+
+function openModal(event) {
+    event.preventDefault();//не даємо відкриватися імг-шці за замовченням
+}
+
+// ініціалізуємо SimpleLightbox
+let lightbox = new SimpleLightbox('.gallery a', {
+    // close: false,
+    // showCounter: false,
+    // captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    // captionPosition: 'bottom',
+});
+

7595ab5da15e9a436097cc11d24ae8a2427edf5e
YurGoV <yurgov@gmail.com>
2022-10-06 15:39:08 +0300

    2-nd task draft

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,41 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const lastPlayTimeJson = localStorage.getItem("videoplayer-current-time");
+
+const lastPlayTime = JSON.parse(lastPlayTimeJson);
+
+
+console.log(lastPlayTimeJson);
+console.log(lastPlayTime);
+
 player.on('play', function() {
     console.log('played the video!');
+
+
+
 });
+
+player.setCurrentTime(lastPlayTime);
+
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
 
-// todo - install lodash
+
+player.on('timeupdate', throttle(currentPlayTime, 1000));
+
+function currentPlayTime(currendTimeData){
+        // player.on('timeupdate', function(throttle (currentPlayTime, 1000)) {
+            // currendTime = JSON.stringify(currendTimeData.seconds);
+            // const videoplayerCurrentTime = JSON.stringify(currentPlayTime);
+            // console.log(currendTime);
+            console.log(JSON.stringify(currendTimeData.seconds));
+            // localStorage.setItem("videoplayer-current-time", JSON.stringify(currendTime));
+            localStorage.setItem("videoplayer-current-time", JSON.stringify(currendTimeData.seconds));
+}
+
